2cc36fc58ea51ca35e70ef1c4e4b3ed036af03e6
ochiogrande <72050095+ochiogrande@users.noreply.github.com>
2023-02-17 13:33:31 +0000

    New enemy

--- Assets/Olteanu Scripts/Enemies/EnemyByOlteanu.cs
+++ Assets/Olteanu Scripts/Enemies/EnemyByOlteanu.cs
@@ -8,9 +8,65 @@ public class EnemyByOlteanu : MonoBehaviour
     public bool isGrounded;
     public Animator anim;
 
+    public Transform[] patroPoints;
+    private int currentPoint;
+    public float speed, waitAtPoint, jumpForce;
+    private float waitCounter;
+    public Rigidbody2D rb;
+    public bool isOn;
+
+
     private void Start()
     {
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        waitCounter = waitAtPoint;
+        foreach (Transform pPoint in patroPoints)
+        {
+            pPoint.SetParent(null);
+        }
+    }
+
+
+    private void Update()
+    {
+        if (Mathf.Abs(transform.position.x - patroPoints[currentPoint].position.x) > .1f)
+        {
+            if (transform.position.x < patroPoints[currentPoint].position.x)
+            {
+                rb.velocity = new Vector2(speed, rb.velocity.y);
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else
+            {
+                rb.velocity = new Vector2(-speed, rb.velocity.y);
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+
+            if (transform.position.y < patroPoints[currentPoint].position.y - .1f && rb.velocity.y < 0.1f)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                // anim.SetTrigger("jump");
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector2(0f, rb.velocity.y);
+            waitCounter -= Time.deltaTime;
+            if (waitCounter <= 0)
+            {
+                waitCounter = waitAtPoint;
+                currentPoint++;
+                if (currentPoint >= patroPoints.Length)
+                {
+                    currentPoint = 0;
+                }
+            }
+        }
+
+
+        anim.SetBool("grounded", isGrounded);
+        anim.SetFloat("speed", Mathf.Abs(rb.velocity.x));
     }
 
 
@@ -20,6 +76,22 @@ public class EnemyByOlteanu : MonoBehaviour
         {
             isGrounded = true;
         }
+        /*
+        if (collision.collider.CompareTag("Player"))
+        {
+            if (isOn)
+            {
+                FindObjectOfType<CameraController>().thePlayer = null;
+                FindObjectOfType<CameraController>().virtualCamera.m_Follow = FindObjectOfType<CameraController>().spawnPoint;
+                FindObjectOfType<LevelManagerByOlteanu>().spawnPlayer = true;
+                FindObjectOfType<LevelManagerByOlteanu>().ResetPlayer();
+                FindObjectOfType<LevelManagerByOlteanu>().KillPlayer();
+                FindObjectOfType<LevelManagerByOlteanu>().AddDeadPlayer();
+                isOn = false;
+                StartCoroutine(ResetEnemyCo());
+            }
+        }
+        */
     }
 
     private void OnCollisionExit2D(Collision2D collision)
@@ -29,4 +101,12 @@ public class EnemyByOlteanu : MonoBehaviour
             isGrounded = false;
         }
     }
+
+    /*
+    IEnumerator ResetEnemyCo()
+    {
+        yield return new WaitForSeconds(.5f);
+        isOn = true;
+    }
+    */
 }
